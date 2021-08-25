30cc121d380228167ebbe2578e9c0ca3e486d737
Hung Tran <hungtran0203@gmail.com>
2021-08-25 12:25:28 +0700

    inject job data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,21 @@ import React from 'react';
 import { ctx } from '@vl/redata';
 import DIV from '@vl/redata/DIV.macro';
 
-import HomePage from '@uz/unitz-pages/Jobs';
-import withPageContext from '@uz/unitz-pages/withPageContext';
+import JobsPage from '@uz/unitz-pages/Jobs';
+import withPageContext, { provideData } from '@uz/unitz-pages/withPageContext';
 import App from '@uz/unitz-app-web/UserApp';
 
 import Layout from '@uz/unitz-layout-web/LayoutMain';
 import SEO from '@uz/unitz-layout-web/SEO';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import _ from 'lodash';
+
 import PageData from '../data/PageDataQuery';
 
 const HomeIndex = withPageContext((props) => {
+  const allNodes = useStaticQuery(GbCtfJobsQuery);
+  provideData('jobs', _.get(allNodes, 'allContentfulJobPost.nodes', []));
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -20,7 +25,7 @@ const HomeIndex = withPageContext((props) => {
           <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            <HomePage />
+            <JobsPage />
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
         </DIV>
@@ -28,5 +33,36 @@ const HomeIndex = withPageContext((props) => {
     </App>
   );
 });
-
 export default HomeIndex;
+
+const GbCtfJobsQuery = graphql`
+  query jobsQuery {
+    allContentfulJobPost(filter: { node_locale: { eq: "en-US" } }) {
+      nodes {
+        id: contentful_id
+        displayName
+        slug
+        longText {
+          longText
+        }
+        categories {
+          ... on ContentfulCategory {
+            id: contentful_id
+            displayName
+            slug
+            sys {
+              type
+              contentType {
+                sys {
+                  type
+                  linkType
+                  id
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
