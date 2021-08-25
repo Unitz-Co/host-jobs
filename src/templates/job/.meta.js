990dfd7f3cb9616030a0f8447ecfc07bef3c7b20
Hung Tran <hungtran0203@gmail.com>
2021-08-25 10:43:34 +0700

    job detail page

---
+++ src/templates/job/.meta.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const _ = require('lodash');
+const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
+const { withLocale } = require('@uz/mod-translations/utils');
+
+exports.createPages = withLocale(async function(item, gatsby) {
+  const localeConfig = this;
+  // @update query
+  const allNodes = await gatsby.graphql(`
+  query jobsQuery {
+    allContentfulJobPost(filter: { node_locale: { eq: "${localeConfig.get('locale')}" } }) {
+      nodes {
+        id: contentful_id
+        displayName
+        avatarUrl {
+          id
+        }
+        slug
+        images {
+          fixed(width: 1600) {
+            width
+            height
+            src
+            srcSet
+          }
+        }
+        image {
+          fixed(width: 1600) {
+            width
+            height
+            src
+            srcSet
+          }
+        }
+      }
+    }
+  }`);
+
+  const jobs = _.get(allNodes, 'data.allContentfulJobPost.nodes', []);
+
+  return Promise.all(
+    jobs.map((cat) => {
+      const catSlug = routeStore.toUrl('category', cat);
+      const catPath = localeConfig.langSlug(path.join('/', catSlug));
+      console.log('creating page', catPath);
+      return gatsby.actions.createPage({
+        path: catPath,
+        component: item.resolvers.component(gatsby),
+        context: {
+          id: _.get(cat, 'id', 'id'),
+          slug: catSlug,
+          params: {
+            ...cat,
+          },
+        },
+      });
+    })
+  );
+});
